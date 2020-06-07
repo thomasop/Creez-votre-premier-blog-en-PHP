b6b5123861645df1b0864c7b1c8f11a59554ccff
thomasop <tdss33@hotmail.com>
2020-06-07 09:38:30 +0200

    Build: add template for views

---
+++ Views/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+        <link href="Public/bootstrap.min.css" rel="stylesheet" /> 
+        <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+               -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+ 
+    </head>
+        
+    <body>
+    
+        <?= $content ?>
+    </body>
+</html>
